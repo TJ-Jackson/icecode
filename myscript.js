b9e195a083d77f64453b54c624dcb0b7c519988e
TJ-Jackson <97995166+TJ-Jackson@users.noreply.github.com>
2022-02-06 16:10:56 -0500

    Add all iniital files for Ice Code

---
+++ myscript.js
@@ -0,0 +1,39 @@
+function showDetermination() {
+    document.getElementById("Determination").style.display="block"; /* display */
+    document.getElementById("Leadership").style.display="none";
+    document.getElementById("Support").style.display="none";
+    document.getElementById("Hardworker").style.display="none";
+    document.getElementById("Fun").style.display="none";
+}
+
+function showLeadership() {
+    document.getElementById("Determination").style.display="none";
+    document.getElementById("Leadership").style.display="block";  /* display */
+    document.getElementById("Support").style.display="none";
+    document.getElementById("Hardworker").style.display="none";
+    document.getElementById("Fun").style.display="none";
+}
+
+function showSupport() {
+    document.getElementById("Determination").style.display="none";
+    document.getElementById("Leadership").style.display="none";
+    document.getElementById("Support").style.display="block";  /* display */
+    document.getElementById("Hardworker").style.display="none";
+    document.getElementById("Fun").style.display="none";
+}
+
+function showHardwork() {
+    document.getElementById("Determination").style.display="none";
+    document.getElementById("Leadership").style.display="none";
+    document.getElementById("Support").style.display="none";
+    document.getElementById("Hardworker").style.display="block";  /* display */
+    document.getElementById("Fun").style.display="none";
+}
+
+function showFun() {
+    document.getElementById("Determination").style.display="none";
+    document.getElementById("Leadership").style.display="none";
+    document.getElementById("Support").style.display="none";
+    document.getElementById("Hardworker").style.display="none";
+    document.getElementById("Fun").style.display="block";  /* display */
+}
